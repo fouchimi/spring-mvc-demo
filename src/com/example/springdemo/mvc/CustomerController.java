da035c14d70c187424554e156d8136f406093038
Fouchimi <ousmanefouchimi@gmail.com>
2017-11-26 17:31:09 -0700

    Added white space validation using @InitBinder with StringTrimmerEditor

--- src/com/example/springdemo/mvc/CustomerController.java
+++ src/com/example/springdemo/mvc/CustomerController.java
@@ -2,9 +2,12 @@ package com.example.springdemo.mvc;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,6 +15,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/customer")
 public class CustomerController {
 
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
+	
 	@RequestMapping("/showForm")
 	public String showForm(Model theModel) {
 		theModel.addAttribute("customer", new Customer());
@@ -20,6 +29,7 @@ public class CustomerController {
 	
 	@RequestMapping("/processForm")
 	public String processForm(@Valid @ModelAttribute("customer") Customer customer, BindingResult theBindingResult) {
+		System.out.println("Customer last name: |" + customer.getLastName() + "|");
 		if(theBindingResult.hasErrors()) return "customer-form";
 		else return "customer-confirmation";
 	}
